916850635e662b2cf52386d5bfc7b54783eebb33
Elleined <demadegu@gmail.com>
2023-08-05 22:34:53 +0800

    remove save tracker in modaltracker api

--- src/main/java/com/forum/application/controller/ModalTrackerController.java
+++ src/main/java/com/forum/application/controller/ModalTrackerController.java
@@ -1,7 +1,5 @@
 package com.forum.application.controller;
 
-import com.forum.application.dto.ModalTrackerDTO;
-import com.forum.application.mapper.ModalTrackerMapper;
 import com.forum.application.model.ModalTracker;
 import com.forum.application.service.ModalTrackerService;
 import lombok.RequiredArgsConstructor;
@@ -14,18 +12,9 @@ import org.springframework.web.bind.annotation.*;
 public class ModalTrackerController {
 
     private final ModalTrackerService modalTrackerService;
-    private final ModalTrackerMapper modalTrackerMapper;
-    @PostMapping("/saveTracker")
-    public ModalTrackerDTO saveTracker(@PathVariable("currentUserId") int receiverId,
-                                       @RequestParam("associatedTypeId") int associateTypeId,
-                                       @RequestParam("type") String type) {
-
-        ModalTracker modalTracker = modalTrackerService.saveTrackerOfUserById(receiverId, associateTypeId, type);
-        return modalTrackerMapper.toDTO(modalTracker);
-    }
 
     @DeleteMapping("/deleteTracker")
-    public ResponseEntity<ModalTrackerDTO> deleteTrackerByUserId(@PathVariable("currentUserId") int currentUserId,
+    public ResponseEntity<ModalTracker> deleteTrackerByUserId(@PathVariable("currentUserId") int currentUserId,
                                                               @RequestParam("type") String type) {
         modalTrackerService.deleteTrackerOfUserById(currentUserId, ModalTracker.Type.valueOf(type));
         return ResponseEntity.noContent().build();
--- src/main/java/com/forum/application/dto/ModalTrackerDTO.java
+++
@@ -1,8 +0,0 @@
-package com.forum.application.dto;
-
-public record ModalTrackerDTO(
-        int receiverId,
-        String type,
-        int associatedTypeIdOpened
-) {
-}
