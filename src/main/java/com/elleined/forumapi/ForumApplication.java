48047789bc079880e18af18aa8a5bf0fa760b76b
Elleined <demadegu@gmail.com>
2023-08-29 01:32:51 +0800

    removed discovery client will be added later

--- src/main/java/com/elleined/forumapi/ForumApplication.java
+++ src/main/java/com/elleined/forumapi/ForumApplication.java
@@ -2,12 +2,10 @@ package com.elleined.forumapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableDiscoveryClient
 public class ForumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
