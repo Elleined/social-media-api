ff8f404039ab8a60f6385cc9a2276826c4b530e6
Elleined <demadegu@gmail.com>
2023-11-06 23:15:04 +0800

    added follow service and some get all methods

--- src/main/java/com/elleined/forumapi/service/follow/FollowService.java
+++ src/main/java/com/elleined/forumapi/service/follow/FollowService.java
@@ -1,4 +1,13 @@
 package com.elleined.forumapi.service.follow;
 
+import com.elleined.forumapi.model.User;
+
+import java.util.List;
+
 public interface FollowService {
+    void follow(User currentUser, User userToFollow);
+    void unFollow(User currentUser, User userToUnFollow);
+
+    List<User> getAllFollowers(User currentUser);
+    List<User> getAllFollowing(User currentUser);
 }
--- src/main/java/com/elleined/forumapi/service/friend/FriendService.java
+++ src/main/java/com/elleined/forumapi/service/friend/FriendService.java
@@ -2,7 +2,12 @@ package com.elleined.forumapi.service.friend;
 
 import com.elleined.forumapi.model.User;
 
+import java.util.List;
+
 public interface FriendService {
     void addFriend(User currentUser, User userToAdd);
     void unFriend(User currentUser, User userToUnFriend);
+
+    List<User> getAllFriends(User currentUser);
+    List<User> getAllFriendRequests(User currentUser);
 }
