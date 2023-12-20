2284ad8abb5c472d16b3d624d724f503977225a1
Elleined <demadegu@gmail.com>
2023-12-20 12:44:52 +0800

    added sendFriendRequest validation for blocked users

--- src/main/java/com/elleined/forumapi/service/friend/FriendService.java
+++ src/main/java/com/elleined/forumapi/service/friend/FriendService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface FriendService {
     void acceptFriendRequest(User currentUser, int friendRequestId);
 
-    void addFriend(User currentUser, User userToAdd);
+    void sendFriendRequest(User currentUser, User userToAdd);
     void unFriend(User currentUser, User userToUnFriend);
 
     List<User> getAllFriends(User currentUser);
--- src/main/java/com/elleined/forumapi/service/friend/FriendServiceImpl.java
+++ src/main/java/com/elleined/forumapi/service/friend/FriendServiceImpl.java
@@ -1,11 +1,13 @@
 package com.elleined.forumapi.service.friend;
 
+import com.elleined.forumapi.exception.BlockedException;
 import com.elleined.forumapi.exception.NotOwnedException;
 import com.elleined.forumapi.exception.ResourceNotFoundException;
 import com.elleined.forumapi.model.User;
 import com.elleined.forumapi.model.friend.FriendRequest;
 import com.elleined.forumapi.repository.FriendRequestRepository;
 import com.elleined.forumapi.repository.UserRepository;
+import com.elleined.forumapi.service.block.BlockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,7 @@ public class FriendServiceImpl implements FriendService {
 
     private final UserRepository userRepository;
     private final FriendRequestRepository friendRequestRepository;
+    private final BlockService blockService;
 
 
     @Override
@@ -41,7 +44,10 @@ public class FriendServiceImpl implements FriendService {
     }
 
     @Override
-    public void addFriend(User currentUser, User userToAdd) {
+    public void sendFriendRequest(User currentUser, User userToAdd) {
+        if (blockService.isBlockedBy(currentUser, userToAdd)) throw new BlockedException("Cannot sent friend request! because you blocked the author of this post with id of !" + userToAdd.getId());
+        if (blockService.isYouBeenBlockedBy(currentUser, userToAdd)) throw  new BlockedException("Cannot sent friend request! because this user with id of " + userToAdd.getId() + " already blocked you");
+
         FriendRequest friendRequest = FriendRequest.builder()
                 .createdAt(LocalDateTime.now())
                 .requestingUser(currentUser)
