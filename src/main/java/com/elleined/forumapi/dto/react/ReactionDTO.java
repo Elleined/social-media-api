9b07a3c04339dbf544d8f6409f4f69722e3e30c0
Elleined <demadegu@gmail.com>
2023-12-25 19:00:33 +0800

    added reactiondto

---
+++ src/main/java/com/elleined/forumapi/dto/react/ReactionDTO.java
@@ -0,0 +1,19 @@
+package com.elleined.forumapi.dto.react;
+
+import com.elleined.forumapi.model.NotificationStatus;
+import com.elleined.forumapi.model.emoji.Emoji;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ReactionDTO {
+    private int id;
+    private LocalDateTime createdAt;
+    private int respondentId;
+    private NotificationStatus notificationStatus;
+    private Emoji.Type emojiType;
+    private int entityId; // Post, comment, or reply
+}
