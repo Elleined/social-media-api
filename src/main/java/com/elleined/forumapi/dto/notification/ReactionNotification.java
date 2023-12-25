e00e1f0de4b13eb55054b149f480b8558b1ee137
Elleined <demadegu@gmail.com>
2023-12-25 18:57:50 +0800

    added emoji notification dto

---
+++ src/main/java/com/elleined/forumapi/dto/notification/ReactionNotification.java
@@ -0,0 +1,17 @@
+package com.elleined.forumapi.dto.notification;
+
+import com.elleined.forumapi.model.emoji.Emoji;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ReactionNotification extends Notification {
+
+    private final Emoji.Type emojiType;
+
+    @Builder
+    public ReactionNotification(int id, String message, int receiverId, String respondentPicture, int respondentId, String formattedDate, String formattedTime, String notificationStatus, Emoji.Type emojiType) {
+        super(id, message, receiverId, respondentPicture, respondentId, formattedDate, formattedTime, notificationStatus);
+        this.emojiType = emojiType;
+    }
+}
