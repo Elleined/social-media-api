02f8bc3b67228d54e7fc9b9d95255dd59cd5e76e
Elleined <demadegu@gmail.com>
2024-01-08 23:11:38 +0800

    added hashtagservice

--- src/main/java/com/elleined/forumapi/service/hashtag/HashTagService.java
+++ src/main/java/com/elleined/forumapi/service/hashtag/HashTagService.java
@@ -4,12 +4,13 @@ import com.elleined.forumapi.model.Post;
 import com.elleined.forumapi.model.hashtag.HashTag;
 
 import java.util.List;
+import java.util.Set;
 
 // String will be replace by entity
 public interface HashTagService {
     List<HashTag> getAll(Post post);
     List<HashTag> getAllByKeyword(String keyword);
-    List<Post> searchByKeyword(String keyword);
+    Set<Post> searchByKeyword(String keyword);
     HashTag save(Post post, String keyword);
     List<HashTag> save(Post post, List<String> word);
 }
