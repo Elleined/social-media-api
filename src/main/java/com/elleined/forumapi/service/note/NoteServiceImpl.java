a33b4c2e3016637f8b653ed58d65b888e0122989
Elleined <demadegu@gmail.com>
2024-01-22 02:01:56 +0800

    added note service implementations

--- src/main/java/com/elleined/forumapi/service/note/NoteServiceImpl.java
+++ src/main/java/com/elleined/forumapi/service/note/NoteServiceImpl.java
@@ -8,6 +8,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -19,26 +23,41 @@ public class NoteServiceImpl implements NoteService {
 
     @Override
     public Note save(User currentUser, String thought) {
-
+        Note note = noteMapper.toEntity(currentUser, thought);
+        noteRepository.save(note);
+        log.debug("Note with id of {} saved successfully!", note.getId());
     }
 
     @Override
     public void update(User currentUser, String newThought) {
-
+        Note note = currentUser.getNote();
+        note.setThought(newThought);
+        noteRepository.save(note);
+        log.debug("Note with id of {} updated with new thought of {}", note.getId(), newThought);
     }
 
     @Override
     public void delete(User currentUser) {
-
+        Note note = currentUser.getNote();
+        noteRepository.delete(note);
+        log.debug("Note with id of {} deleted successfully", note.getId());
     }
 
     @Override
     public Note getNote(User currentUser) {
-        return null;
+        return currentUser.getNote();
     }
 
     @Override
     public void deleteExpiredNotes() {
+        List<Note> notes = noteRepository.findAll().stream()
+                .filter(note -> {
+                    LocalDateTime noteCreation = note.getCreatedAt();
+                    LocalDateTime noteExpiration = noteCreation.plusDays(1);
 
+                    return LocalDateTime.now().isAfter(noteExpiration) ||
+                            LocalDateTime.now().equals(noteExpiration);
+                })
+                .toList();
     }
 }
