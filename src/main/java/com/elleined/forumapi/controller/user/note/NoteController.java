0a710ce2037e45eee8c0f98bf2f61d3c67a3647d
Elleined <demadegu@gmail.com>
2024-01-22 02:12:23 +0800

    added note controller implementations

--- src/main/java/com/elleined/forumapi/controller/user/note/NoteController.java
+++ src/main/java/com/elleined/forumapi/controller/user/note/NoteController.java
@@ -6,7 +6,9 @@ import com.elleined.forumapi.model.note.Note;
 import com.elleined.forumapi.service.UserService;
 import com.elleined.forumapi.service.note.NoteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,23 +20,27 @@ public class NoteController {
     private final NoteService noteService;
     private final NoteMapper noteMapper;
 
-    public Note save(User currentUser, String thought) {
-        return null;
+    public Note save(@PathVariable("currentUserId") int currentUserId,
+                     @RequestParam("thought") String thought) {
+
+        User currentUser = userService.getById(currentUserId);
+        return noteService.save(currentUser, thought);
     }
 
-    public void update(User currentUser, String newThought) {
+    public void update(@PathVariable("currentUserId") int currentUserId,
+                       @RequestParam("newThought") String newThought) {
 
+        User currentUser = userService.getById(currentUserId);
+        noteService.update(currentUser, newThought);
     }
 
-    public void delete(User currentUser) {
-
+    public void delete(@PathVariable("currentUserId") int currentUserId) {
+        User currentUser = userService.getById(currentUserId);
+        noteService.delete(currentUser);
     }
 
-    public Note getNote(User currentUser) {
-        return null;
-    }
-
-    public void deleteExpiredNotes() {
-
+    public Note getNote(@PathVariable("currentUserId") int currentUserId) {
+        User currentUser = userService.getById(currentUserId);
+        return noteService.getNote(currentUser);
     }
 }
