84bd8fbaa4a7481d689fd3b070adbed24322180e
Elleined <demadegu@gmail.com>
2024-02-03 21:00:07 +0800

    created test class for commenttmention

---
+++ src/test/java/com/elleined/forumapi/model/mention/CommentMentionTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CommentMentionTest {
+  
+}
