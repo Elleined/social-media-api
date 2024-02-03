828a5a033addad20aa96fed5bee65bb93b7be418
Elleined <demadegu@gmail.com>
2024-02-03 22:02:03 +0800

    created test class for post  model class

--- src/test/java/com/elleined/forumapi/ForumApplicationTests.java
+++ src/test/java/com/elleined/forumapi/ForumApplicationTests.java
@@ -3,11 +3,5 @@ package com.elleined.forumapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ForumApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
