a65b2fdf4bc682cbc480b9e22cc6cecbabd7b8a1
Elleined <demadegu@gmail.com>
2024-02-25 22:38:14 +0800

    tested wsservice

---
+++ src/test/java/com/elleined/socialmediaapi/service/ModalTrackerServiceTest.java
@@ -0,0 +1,97 @@
+package com.elleined.socialmediaapi.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ModalTrackerServiceTest {
+
+    @Test
+    void saveTrackerOfUserById() {
+        // Expected Value
+
+        // Mock data
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Behavior Verifications
+
+        // Assertions
+    }
+
+    @Test
+    void getTrackerOfUserById() {
+        // Expected Value
+
+        // Mock data
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Behavior Verifications
+
+        // Assertions
+    }
+
+    @Test
+    void deleteTrackerOfUserById() {
+        // Expected Value
+
+        // Mock data
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Behavior Verifications
+
+        // Assertions
+    }
+
+    @Test
+    void isModalOpen() {
+        // Expected Value
+
+        // Mock data
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Behavior Verifications
+
+        // Assertions
+    }
+
+    @Test
+    void deleteAll() {
+        // Expected Value
+
+        // Mock data
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Behavior Verifications
+
+        // Assertions
+    }
+}
