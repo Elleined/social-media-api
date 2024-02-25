55644e8169232764fd8a85ef38d75f82830d6221
Elleined <demadegu@gmail.com>
2024-02-25 22:21:15 +0800

    tested wsservice

--- src/main/java/com/elleined/socialmediaapi/dto/CommentDTO.java
+++ src/main/java/com/elleined/socialmediaapi/dto/CommentDTO.java
@@ -1,13 +1,17 @@
 package com.elleined.socialmediaapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDTO {
     private int id;
     private String body;
