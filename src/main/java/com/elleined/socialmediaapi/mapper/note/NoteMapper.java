6bad25cce1ff2ef4c4129128070838d02287f3d9
Elleined <demadegu@gmail.com>
2024-05-15 21:55:18 +0800

    added note mapper

---
+++ src/main/java/com/elleined/socialmediaapi/mapper/note/NoteMapper.java
@@ -0,0 +1,34 @@
+package com.elleined.socialmediaapi.mapper.note;
+
+import com.elleined.socialmediaapi.dto.note.NoteDTO;
+import com.elleined.socialmediaapi.mapper.CustomMapper;
+import com.elleined.socialmediaapi.model.note.Note;
+import com.elleined.socialmediaapi.model.user.User;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface NoteMapper extends CustomMapper<Note, NoteDTO> {
+
+    @Override
+    @Mappings({
+            @Mapping(target = "id", source = "id"),
+            @Mapping(target = "createdAt", source = "createdAt"),
+            @Mapping(target = "updatedAt", source = "updatedAt"),
+            @Mapping(target = "thought", source = "thought"),
+            @Mapping(target = "creatorId", source = "creator.id")
+    })
+    NoteDTO toDTO(Note note);
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())"),
+            @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())"),
+            @Mapping(target = "thought", expression = "java(thought)"),
+            @Mapping(target = "creator", expression = "java(creator)")
+    })
+    Note toEntity(User creator,
+                  @Context String thought);
+}
