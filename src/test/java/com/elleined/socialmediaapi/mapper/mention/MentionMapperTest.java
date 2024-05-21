3569c6698e2069ba917dbb6b0636d92659053e9d
Elleined <demadegu@gmail.com>
2024-05-21 21:49:57 +0800

    Tested mentionmapper

--- src/test/java/com/elleined/socialmediaapi/mapper/mention/MentionMapperTest.java
+++ src/test/java/com/elleined/socialmediaapi/mapper/mention/MentionMapperTest.java
@@ -3,36 +3,76 @@ package com.elleined.socialmediaapi.mapper.mention;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import com.elleined.socialmediaapi.dto.mention.MentionDTO;
+import com.elleined.socialmediaapi.model.mention.Mention;
+import com.elleined.socialmediaapi.model.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
 
 @ExtendWith(MockitoExtension.class)
 class MentionMapperTest {
 
+    private final MentionMapper mentionMapper = Mappers.getMapper(MentionMapper.class);
+
     @Test
-    @DisplayName("")
+    @DisplayName("to DTO")
     void toDTO() {
         // Pre defined values
 
         // Expected Value
 
         // Mock data
+        Mention expected = Mention.builder()
+                .id(1)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .creator(User.builder()
+                        .id(1)
+                        .build())
+                .mentionedUser(User.builder()
+                        .id(1)
+                        .build())
+                .posts(new HashSet<>())
+                .comments(new HashSet<>())
+                .replies(new HashSet<>())
+                .notifications(new HashSet<>())
+                .build();
 
         // Set up method
 
         // Stubbing methods
 
         // Calling the method
+        MentionDTO actual = mentionMapper.toDTO(expected);
 
         // Behavior Verifications
 
         // Assertions
+        assertEquals(expected.getId(), actual.getId());
+        assertNotNull(actual.getCreatedAt());
+        assertNotNull(actual.getUpdatedAt());
+
+        assertEquals(1, actual.getCreatorId());
+
+        assertEquals(1, actual.getMentionedUserId());
+
+        assertNotNull(actual.getPostIds());
+
+        assertNotNull(actual.getCommentIds());
+
+        assertNotNull(actual.getReplyIds());
+
+        assertNotNull(actual.getNotificationIds());
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("to entity")
     void toEntity() {
         // Pre defined values
 
@@ -45,9 +85,25 @@ class MentionMapperTest {
         // Stubbing methods
 
         // Calling the method
+        Mention expected = mentionMapper.toEntity(new User(), new User());
 
         // Behavior Verifications
 
         // Assertions
+        assertEquals(0, expected.getId());
+        assertNotNull(expected.getCreatedAt());
+        assertNotNull(expected.getUpdatedAt());
+
+        assertNotNull(expected.getCreator());
+
+        assertNotNull(expected.getMentionedUser());
+
+        assertNotNull(expected.getPosts());
+
+        assertNotNull(expected.getComments());
+
+        assertNotNull(expected.getAllReplyIds());
+
+        assertNotNull(expected.getAllNotificationIds());
     }
 }
