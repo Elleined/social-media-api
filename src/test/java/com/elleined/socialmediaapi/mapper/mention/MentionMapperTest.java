94f731ebab7d6e5b1d547790856907e27aed6700
Elleined <demadegu@gmail.com>
2024-05-21 21:40:58 +0800

    Tested hashtagmapper

---
+++ src/test/java/com/elleined/socialmediaapi/mapper/mention/MentionMapperTest.java
@@ -0,0 +1,53 @@
+package com.elleined.socialmediaapi.mapper.mention;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MentionMapperTest {
+
+    @Test
+    @DisplayName("")
+    void toDTO() {
+        // Pre defined values
+
+        // Expected Value
+
+        // Mock data
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Behavior Verifications
+
+        // Assertions
+    }
+
+    @Test
+    @DisplayName("")
+    void toEntity() {
+        // Pre defined values
+
+        // Expected Value
+
+        // Mock data
+
+        // Set up method
+
+        // Stubbing methods
+
+        // Calling the method
+
+        // Behavior Verifications
+
+        // Assertions
+    }
+}
