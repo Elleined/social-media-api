b74d39e56ab2da87ca44d4f66347e90a62bba9b2
Elleined <demadegu@gmail.com>
2024-06-14 21:24:43 +0800

    Added note reaction notification

---
+++ src/main/java/com/elleined/socialmediaapi/controller/notification/reaction/NoteReactionNotificationController.java
@@ -0,0 +1,54 @@
+package com.elleined.socialmediaapi.controller.notification.reaction;
+
+import com.elleined.socialmediaapi.dto.notification.reaction.NoteReactionNotificationDTO;
+import com.elleined.socialmediaapi.mapper.notification.reaction.ReactionNotificationMapper;
+import com.elleined.socialmediaapi.model.note.Note;
+import com.elleined.socialmediaapi.model.notification.Notification;
+import com.elleined.socialmediaapi.model.notification.reaction.NoteReactionNotification;
+import com.elleined.socialmediaapi.model.user.User;
+import com.elleined.socialmediaapi.service.notification.reaction.ReactionNotificationService;
+import com.elleined.socialmediaapi.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users/{currentUserId}/note-reaction-notifications")
+public class NoteReactionNotificationController {
+
+    private final UserService userService;
+
+    private final ReactionNotificationService<NoteReactionNotification, Note> reactionNotificationService;
+    private final ReactionNotificationMapper reactionNotificationMapper;
+
+    @GetMapping
+    public List<NoteReactionNotificationDTO> getAll(@PathVariable("currentUserId") int currentUserId,
+                                                    @RequestParam("status") Notification.Status status,
+                                                    @RequestParam(required = false, defaultValue = "1", value = "pageNumber") int pageNumber,
+                                                    @RequestParam(required = false, defaultValue = "5", value = "pageSize") int pageSize,
+                                                    @RequestParam(required = false, defaultValue = "ASC", value = "sortDirection") Sort.Direction direction,
+                                                    @RequestParam(required = false, defaultValue = "id", value = "sortBy") String sortBy) {
+
+        User currentUser = userService.getById(currentUserId);
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, direction, sortBy);
+
+        return reactionNotificationService.getAll(currentUser, status, pageable).stream()
+                .map(reactionNotificationMapper::toDTO)
+                .toList();
+    }
+
+    @PostMapping("/{id}/read")
+    public void read(@PathVariable("currentUserId") int currentUserId,
+                     @PathVariable("id") int id) {
+
+        User currentUser = userService.getById(currentUserId);
+        NoteReactionNotification noteReactionNotification = reactionNotificationService.getById(id);
+
+        reactionNotificationService.read(currentUser, noteReactionNotification);
+    }
+}
