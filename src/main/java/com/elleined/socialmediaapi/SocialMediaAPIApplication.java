3ec4e38bd24e8d73067ecb095e5a8acac36179a8
Elleined <demadegu@gmail.com>
2024-07-16 19:03:34 +0800

    Added eureka discovery client

--- src/main/java/com/elleined/socialmediaapi/SocialMediaAPIApplication.java
+++ src/main/java/com/elleined/socialmediaapi/SocialMediaAPIApplication.java
@@ -2,16 +2,21 @@ package com.elleined.socialmediaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+@EnableCaching
 @EnableScheduling
-@SpringBootApplication
+@EnableDiscoveryClient
 @EnableTransactionManagement
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SocialMediaAPIApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaAPIApplication.class, args);
